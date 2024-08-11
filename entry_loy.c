ebe9b1e60661dc3f06d1416ddedd7c4b3c4c0da3
Loy <loytakian@gmail.com>
2024-08-11 14:38:13 -0300

    CLASS: Basic Player Movement
    Finish the class Basic Player Movement from Skool.

---
+++ entry_loy.c
@@ -0,0 +1,71 @@
+
+int entry(int argc, char **argv) {
+	
+	window.title = STR("Randy's Game");
+	window.width = 1280;
+	window.height = 720;
+	window.x = 200;
+	window.y = 200;
+	window.clear_color = hex_to_rgba(0x2a2d3aff);
+	
+	Gfx_Image* player = load_image_from_disk(STR("../sprites/player.png"), get_heap_allocator());
+	assert(player, "fuckie wucky happen");
+
+
+	Vector2 player_pos = v2(0, 0);
+	float64 seconds_counter = 0.0;
+	s32 frame_count = 0;
+
+	float64 last_time = os_get_current_time_in_seconds();
+	while (!window.should_close) {
+		reset_temporary_storage();
+
+		draw_frame.projection = m4_make_orthographic_projection(window.width * -0.5, window.width * 0.5, window.height * -0.5, window.height * 0.5, -1, 10);
+		float zoom = 5.3;
+		draw_frame.view = m4_make_scale(v3(1.0/zoom, 1.0/zoom, 1.0));
+
+		float64 now = os_get_current_time_in_seconds();
+		float64 delta_t = now - last_time;
+		last_time = now;
+		
+		os_update();
+
+		if (is_key_just_pressed(KEY_ESCAPE)) {
+			window.should_close = true;
+		}
+
+		Vector2 input_axis = v2(0, 0);
+		if (is_key_down('A')) {
+			input_axis.x -= 1.0;
+		}
+		if (is_key_down('D')) {
+			input_axis.x += 1.0;
+		}
+		if (is_key_down('S')) {
+			input_axis.y -= 1.0;
+		}
+		if (is_key_down('W')) {
+			input_axis.y += 1.0;
+		}
+		input_axis = v2_normalize(input_axis);
+
+		player_pos = v2_add(player_pos, v2_mulf(input_axis, 100.0 * delta_t));
+		
+		Vector2 size = v2(6.0, 8.0);
+		Matrix4 xform = m4_scalar(1.0);
+		xform         = m4_translate(xform, v3(player_pos.x, player_pos.y, 0));
+		xform         = m4_translate(xform, v3(size.x * -0.5, 0.0, 0));
+		draw_image_xform(player, xform, size, COLOR_RED);
+		
+		gfx_update();
+		seconds_counter += delta_t;
+		frame_count += 1;
+		if (seconds_counter > 1.0) {
+			log("fps: %i", frame_count);
+			seconds_counter = 0.0;
+			frame_count = 0;
+		}
+	}
+
+	return 0;
+}
